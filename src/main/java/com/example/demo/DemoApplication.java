5a280690f3199429a85196698a78b34fc2a355f9
newkumpon <nontapap.th@gmail.com>
2019-10-22 16:25:37 +0700

    edit application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,5 +17,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+		System.out.println("[KtbLineNotiApplication][START]handleTextMessage");
+		System.out.println("[KtbLineNotiApplication]" + e);
+        TextMessageContent message = e.getMessage();
+        System.out.println("[KtbLineNotiApplication][END]handleTextMessage");
+        return new TextMessage(message.getText());
+	}
 
 }
