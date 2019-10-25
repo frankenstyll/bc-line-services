35748b16834f62238a82b6e4c78b53d2b0ef316a
newkumpon <nontapap.th@gmail.com>
2019-10-25 17:35:19 +0700

    rollback

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -13,6 +14,8 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.quickreply.QuickReply;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -30,7 +33,7 @@ public class BotController {
 	@Autowired
 	private MessageSource messageSource;
 
-	/*
+	
 	@EventMapping
 	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
 		System.out.println("[BotController][START]handleTextMessage");
@@ -38,27 +41,51 @@ public class BotController {
         TextMessageContent message = e.getMessage();
         System.out.println("[BotController][END]handleTextMessage");
         return new TextMessage("[BotController]"+message.getText());
-	}
-	*/
 	
-	@EventMapping
-	public void handleTextMessage(MessageEvent<TextMessageContent> event) {
-		TextMessageContent message = event.getMessage();
-		handleTextContent(event.getReplyToken(), event, message);
 	}
 	
-	private void handleTextContent(String replyToken, Event event, TextMessageContent content) {				
+//	@EventMapping
+//	public void handleTextMessage(MessageEvent<TextMessageContent> event) {
+//		TextMessageContent message = event.getMessage();
+//		handleTextContent(event.getReplyToken(), event, message);
+//	}
+//	
+//	private void handleTextContent(String replyToken, Event event, TextMessageContent content) {				
 		
-		try{			
-			
-			String text = content.getText();
-			String userId = event.getSource().getUserId();
-			List<Message> mes = new ArrayList<Message>();
-			
-			lineMessagingClient.pushMessage(new PushMessage(userId, mes));
-			
-		}catch(Exception e){			
-			e.printStackTrace();
-		}
-	}
+//		final LineMessagingClient client = LineMessagingClient
+//		        .builder("<channel access token>")
+//		        .build();
+//
+//		
+//		final TextMessage textMessage = new TextMessage("hello");
+//		final PushMessage pushMessage = new PushMessage("<to>",textMessage);
+//
+//		final BotApiResponse botApiResponse;
+//		try {
+//		    botApiResponse = client.pushMessage(pushMessage).get();
+//		    
+//		} catch (InterruptedException | ExecutionException e) {
+//		    e.printStackTrace();
+//		    return;
+//		}
+//
+//		System.out.println(botApiResponse);
+		
+//		try{			
+//			
+//			String text = content.getText();
+//			String userId = event.getSource().getUserId();
+//			List<Message> mes = new ArrayList<Message>();
+//			
+//			lineMessagingClient.pushMessage(new PushMessage(userId, mes));
+//			
+//		}catch(Exception e){			
+//			e.printStackTrace();
+//		}
+//		
+//		
+//		
+//	}
+	
+
 }
