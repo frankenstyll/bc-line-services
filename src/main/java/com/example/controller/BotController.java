d289534ec07c6c16733463c6372308cec35aed4e
newkumpon <nontapap.th@gmail.com>
2019-10-25 17:45:59 +0700

    update to heroku

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -34,21 +34,40 @@ public class BotController {
 	private MessageSource messageSource;
 
 	
+//	@EventMapping
+//	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+//		System.out.println("[BotController][START]handleTextMessage");
+//		System.out.println("[BotController]" + e);
+//        TextMessageContent message = e.getMessage();
+//        System.out.println("[BotController][END]handleTextMessage");
+//        return new TextMessage("[BotController]"+message.getText());
+//	
+//	}
+	
 	@EventMapping
-	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
-		System.out.println("[BotController][START]handleTextMessage");
-		System.out.println("[BotController]" + e);
-        TextMessageContent message = e.getMessage();
-        System.out.println("[BotController][END]handleTextMessage");
-        return new TextMessage("[BotController]"+message.getText());
+	public void handleTextMessage(MessageEvent<TextMessageContent> event) {
+		TextMessageContent message = event.getMessage();
+		final LineMessagingClient client = LineMessagingClient
+		        .builder("01fd9f585adb28aeafc27432da653d5f")
+		        .build();
+
+		
+		final TextMessage textMessage = new TextMessage("hello");
+		final PushMessage pushMessage = new PushMessage("Ue0f8a270ffea40064588037b51272d28",textMessage);
+		
+		
+		final BotApiResponse botApiResponse;
+		try {
+		    botApiResponse = client.pushMessage(pushMessage).get();
+		    
+		} catch (InterruptedException | ExecutionException e) {
+		    e.printStackTrace();
+		    return;
+		}
+
+		System.out.println(botApiResponse);
 	
 	}
-	
-//	@EventMapping
-//	public void handleTextMessage(MessageEvent<TextMessageContent> event) {
-//		TextMessageContent message = event.getMessage();
-//		handleTextContent(event.getReplyToken(), event, message);
-//	}
 //	
 //	private void handleTextContent(String replyToken, Event event, TextMessageContent content) {				
 		
@@ -70,7 +89,7 @@ public class BotController {
 //		}
 //
 //		System.out.println(botApiResponse);
-		
+//		
 //		try{			
 //			
 //			String text = content.getText();
