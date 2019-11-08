5571f96b50ee663fdbb69b01b460f70aff51d1b8
newkumpon <nontapap.th@gmail.com>
2019-11-08 17:45:06 +0700

    add broadcast message

--- src/main/java/com/ktb/leadandsales/controller/test/ReceiveMessage.java
+++ src/main/java/com/ktb/leadandsales/controller/test/ReceiveMessage.java
@@ -78,7 +78,7 @@ public class ReceiveMessage {
 			log.info("Call push message");
 			log.info("USERID : " + messageBean.getUserId());
 			log.info(""+messageBean.getMessages());
-			lineService.handleBroadCastMessageContent(messageBean.getUserId() , messageBean.getMessages());
+			lineService.handleBroadCastMessageContent("userid text" , messageBean.getMessages());
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
